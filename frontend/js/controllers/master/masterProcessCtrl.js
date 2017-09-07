6a158a5c1f2d2e1653c186f423259d6490499a57
Pratik Shah <antu.wohligtech@gmail.com>
2017-09-07 11:22:08 +0530

    masters modal

--- frontend/js/controllers/master/masterProcessCtrl.js
+++ frontend/js/controllers/master/masterProcessCtrl.js
@@ -1,3 +1,16 @@
 myApp.controller('masterProcessCtrl', function ($rootScope, $scope, $http, $timeout, $uibModal) {
     $scope.$parent.isSidebarActive = true;
+   
+    //modal start
+  $scope.createorEditUom = function () {
+            $scope.loginModal = $uibModal.open({
+                animation: true,
+                templateUrl: 'views/modal/createorEditUom.html',
+                scope: $scope,
+                size: 'lg',
+                windowClass: "bael-modal"
+
+            });
+        };
+         //end of modal
 });
