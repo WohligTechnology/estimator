811402e9d4c959d04efb78eccda7166458cbee75
Pratik Shah <antu.wohligtech@gmail.com>
2017-09-08 15:06:12 +0530

    masters popups

--- frontend/js/controllers/master/baseMatserCtrl.js
+++ frontend/js/controllers/master/baseMatserCtrl.js
@@ -1,3 +1,43 @@
 myApp.controller('baseMatserCtrl', function ($rootScope, $scope, $http, $timeout, $uibModal) {
     $scope.$parent.isSidebarActive = true;
+    // Uom modal start
+    $scope.createOrEditUom = function () {
+        $scope.loginModal = $uibModal.open({
+            animation: true,
+            templateUrl: 'views/modal/createOrEditUom.html',
+            scope: $scope,
+            size: 'md',
+        });
+    };
+    //end of modal
+      //Variable modal start
+    $scope.variable = function () {
+        $scope.loginModal = $uibModal.open({
+            animation: true,
+            templateUrl: 'views/modal/createOrEditVariable.html',
+            scope: $scope,
+            size: 'md',
+        });
+    };
+    //end of modal
+    //Formulas modal start
+    $scope.formulas = function () {
+        $scope.loginModal = $uibModal.open({
+            animation: true,
+            templateUrl: 'views/modal/createOrEditFormulas.html',
+            scope: $scope,
+            size: 'md',
+        });
+    };
+    //end of modal
+        //Markups modal start
+    $scope.markups = function () {
+        $scope.loginModal = $uibModal.open({
+            animation: true,
+            templateUrl: 'views/modal/createOrEditMarkups.html',
+            scope: $scope,
+            size: 'md',
+        });
+    };
+    //end of modal
 });
--- frontend/js/controllers/master/masterMaterialCtrl.js
+++ frontend/js/controllers/master/masterMaterialCtrl.js
@@ -1,3 +1,13 @@
 myApp.controller('masterMaterialCtrl', function ($rootScope, $scope, $http, $timeout, $uibModal) {
     $scope.$parent.isSidebarActive = false;
+     //modal start
+    $scope.material = function () {
+        $scope.loginModal = $uibModal.open({
+            animation: true,
+            templateUrl: 'views/modal/createOrEditMaterial.html',
+            scope: $scope,
+            size: 'md',
+        });
+    };
+    //end of modal
 });
