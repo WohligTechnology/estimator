c578e504b2469d13ea47562cd2572146768d9ae8
Pratik Shah <antu.wohligtech@gmail.com>
2017-09-08 18:22:38 +0530

    masters tree

--- frontend/js/controllers/master/masterMaterialCtrl.js
+++ frontend/js/controllers/master/masterMaterialCtrl.js
@@ -1,6 +1,6 @@
 myApp.controller('masterMaterialCtrl', function ($rootScope, $scope, $http, $timeout, $uibModal) {
     $scope.$parent.isSidebarActive = false;
-     //modal start
+    //modal start
     $scope.material = function () {
         $scope.loginModal = $uibModal.open({
             animation: true,
@@ -10,4 +10,39 @@ myApp.controller('masterMaterialCtrl', function ($rootScope, $scope, $http, $tim
         });
     };
     //end of modal
+
+    $scope.materialCat = [{
+        "name": "materialCat 1",
+        "materialSub": [{
+            "name": "MaterialSub Cat 1",
+            "material": [{
+                "name": "Material 1"
+            }, {
+                "name": "Material 2"
+            }]
+        }, ]
+    }, {
+        "name": "materialCat 2",
+        "materialSub": [{
+            "name": "MaterialSub Cat 1",
+            "material": [{
+                "name": "Material 1"
+            }, {
+                "name": "Material 2"
+            }]
+
+        }, {
+            "name": "MaterialSub Cat 2"
+        }, {
+            "name": "MaterialSub Cat 3"
+        }]
+    }, {
+        "name": "materialCat 3",
+        "materialSub": [{
+            "name": "MaterialSub Cat 1"
+        }]
+    }];
+
+
+
 });
