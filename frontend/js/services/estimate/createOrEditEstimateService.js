4d34d1d2b7c86c324bd03e49be2145e9e9825243
ashishzanwar <ashish.zanwar@wohlig.com>
2017-09-11 10:19:52 +0530

    small changes

--- frontend/js/services/estimate/createOrEditEstimateService.js
+++ frontend/js/services/estimate/createOrEditEstimateService.js
@@ -1,71 +1,6 @@
 myApp
     .service('createOrEditEstimateService', function ($http) {
 
-        this.estimateView = function (estimateView, callback) {
-            if (estimateView == 'estimateAssembly') {
-                getEstimateView = "../frontend/views/content/estimate/estimateViews/assembly.html";
-            } else if (estimateView == 'estimateSubAssembly') {
-                getEstimateView = "../frontend/views/content/estimate/estimateViews/subAssembly.html";
-            } else if (estimateView == 'estimatePartDetail') {
-                getEstimateView = "../frontend/views/content/estimate/estimateViews/partDetail.html";
-            } else if (estimateView == 'estimatePartItemDetail') {
-                getEstimateView = "../frontend/views/content/estimate/estimateViews/editPartItemDetail.html";
-            } else if (estimateView == 'estimateProcessing') {
-                getEstimateView = "../frontend/views/content/estimate/estimateViews/processing.html";
-            } else if (estimateView == 'estimateAddons') {
-                getEstimateView = "../frontend/views/content/estimate/estimateViews/addons.html";
-            } else if (estimateView == 'estimateExtras') {
-                getEstimateView = "../frontend/views/content/estimate/estimateViews/extras.html";
-            } else if (estimateView == 'estimateCustomMaterial') {
-                getEstimateView = "../frontend/views/content/estimate/estimateViews/customMaterial.html";
-            }
-
-            callback(getEstimateView);
-        }
-        this.estimateViewData = function(estimateView,getId,getLevelName, callback){
-            console.log('**** inside estimateViewData of createOrEditEstimateCtrl.js ****');
-            if (estimateView == 'estimateAssembly') {
-                
-            } else if (estimateView == 'estimateSubAssembly') {
-                
-            } else if (estimateView == 'estimatePartDetail') {
-                
-            } else if (estimateView == 'estimatePartItemDetail') {
-                
-            } else if (estimateView == 'estimateProcessing') {
-                
-            } else if (estimateView == 'estimateAddons') {
-                
-            } else if (estimateView == 'estimateExtras') {
-                
-            } else if (estimateView == 'estimateCustomMaterial') {
-                
-            }
-
-            callback();
-        }
-        this.createSubAssemblyModal = function () {
-            console.log('**** inside createSubAssemblyModal of createOrEditEstimateCtrl.js ****');
-        }
-        this.editAssembly = function () {
-            console.log('**** inside editAssembly of createOrEditEstimateCtrl.js ****');
-        }
-        this.createEstimatePart = function () {
-            console.log('**** inside createEstimatePart of createOrEditEstimateCtrl.js ****');
-        }
-        this.editSubAssembly = function () {
-            console.log('**** inside editSubAssembly of createOrEditEstimateCtrl.js ****');
-        }
-        this.deleteSubAssembly = function () {
-            console.log('**** inside deleteSubAssembly of createOrEditEstimateCtrl.js ****');
-        }
-        this.editEstimatePart = function () {
-            console.log('**** inside editEstimatePart of createOrEditEstimateCtrl.js ****');
-        }
-        this.deleteEstimatePart = function () {
-            console.log('**** inside deleteEstimatePart of createOrEditEstimateCtrl.js ****');
-        }
-
         this.getEstimateData = function (callback) {
             var subAssembly = [{
                 "_id": "1111111111111111111111111",
@@ -105,4 +40,70 @@ myApp
             }
             callback(estimateData);
         }
+
+        this.estimateView = function (estimateView, callback) {
+            getEstimateView = "../frontend/views/content/estimate/estimateViews/"+estimateView+".html";
+            callback(getEstimateView);
+        }
+        this.estimateViewData = function (estimateView, getId, getLevelName, callback) {
+
+            if (estimateView == 'assembly') {
+                // get all subAssemblies from API
+                var getAllSubAssemblies = [{}];
+                callback();
+            } else if (estimateView == 'subAssembly') {
+                // get all parts from API
+                var getAllParts = [{}];
+                callback();
+            } else if (estimateView == 'partDetail') {
+                // get part all detail (with processing count, addons count & extras count) from API
+                var getPartDetail = {};
+                callback();
+            } else if (estimateView == 'editPartItemDetail') {
+                // get part Item detail only from API
+                var getPartItemDetail = {};
+                callback();
+            } else if (estimateView == 'processing') {
+                // get all processing of corresponding level (assembly, subAssembly & part) from API
+                var getAllProcessing = [{}];
+                callback();
+            } else if (estimateView == 'addons') {
+                // get all addons of corresponding level (assembly, subAssembly & part) from API
+                var getAllAddons = [{}];
+                callback();
+            } else if (estimateView == 'extras') {
+                // get all extras of corresponding level (assembly, subAssembly & part) from API
+                var getAllEtras = [{}];
+                callback();
+            } else if (estimateView == 'customMaterial') {
+                // get all custome materials of estimate from API
+                var getAllCustomMaterial = [{}];
+                callback();
+            }
+
+        }
+
+        this.createSubAssemblyModal = function () {
+            console.log('**** inside createSubAssemblyModal of createOrEditEstimateCtrl.js ****');
+        }
+        this.editAssembly = function () {
+            console.log('**** inside editAssembly of createOrEditEstimateCtrl.js ****');
+        }
+        this.createEstimatePart = function () {
+            console.log('**** inside createEstimatePart of createOrEditEstimateCtrl.js ****');
+        }
+        this.editSubAssembly = function () {
+            console.log('**** inside editSubAssembly of createOrEditEstimateCtrl.js ****');
+        }
+        this.deleteSubAssembly = function () {
+            console.log('**** inside deleteSubAssembly of createOrEditEstimateCtrl.js ****');
+        }
+        this.editEstimatePart = function () {
+            console.log('**** inside editEstimatePart of createOrEditEstimateCtrl.js ****');
+        }
+        this.deleteEstimatePart = function () {
+            console.log('**** inside deleteEstimatePart of createOrEditEstimateCtrl.js ****');
+        }
+
+
     });
--- frontend/js/services/master/masterMaterialService.js
+++ frontend/js/services/master/masterMaterialService.js
@@ -1,3 +1,44 @@
-myApp.controller('masterMaterialService', function ($rootScope, $scope, $http, $timeout, $uibModal) {
-    // $scope.$parent.isSidebarActive = true;
+myApp.service('masterMaterialService', function ($http) {
+    //- get master material view
+    this.getMaterialView = function (callback) {
+        callback();
+    }
+    //- get master material tree structure data
+    this.getMaterialData = function (callback) {
+        var materialCat = [{
+            "name": "materialCat 1",
+            "materialSub": [{
+                "name": "MaterialSub Cat 1",
+                "material": [{
+                    "name": "Material 1"
+                }, {
+                    "name": "Material 2"
+                }]
+            }, ]
+        }, {
+            "name": "materialCat 2",
+            "materialSub": [{
+                "name": "MaterialSub Cat 1",
+                "material": [{
+                    "name": "Material 1"
+                }, {
+                    "name": "Material 2"
+                }]
+
+            }, {
+                "name": "MaterialSub Cat 2"
+            }, {
+                "name": "MaterialSub Cat 3"
+            }]
+        }, {
+            "name": "materialCat 3",
+            "materialSub": [{
+                "name": "MaterialSub Cat 1"
+            }]
+        }];
+
+        callback(materialCat);
+    }
+    //- modal to add or edit material category
+
 });
