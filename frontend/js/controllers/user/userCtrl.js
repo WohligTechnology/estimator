efa5ad667463f1c2d52236ed26990764767bc5a1
ashishzanwar <ashish.zanwar@wohlig.com>
2017-09-26 08:36:35 +0530

    partial done wth users crud

--- frontend/js/controllers/user/userCtrl.js
+++ frontend/js/controllers/user/userCtrl.js
@@ -1,91 +1,75 @@
-myApp.controller('userCtrl', function ($rootScope, $scope, $http, $timeout, $uibModal) {
+myApp.controller('userCtrl', function ($scope, $http, $uibModal, userService) {
   $scope.$parent.isSidebarActive = true;
 
-  $scope.$on('$viewContentLoaded', function () {
-    // initialize core components
-    App.initAjax();
-  });
-  //modal Create or Edit user
-   $scope.createOrEdit = function () {
-        $scope.modalInstance = $uibModal.open({
-            animation: true,
-            templateUrl: 'views/content/user/modal/createOrEditUser.html',
-            scope: $scope,
-            size: 'md',
-        });
-    };
-     //modal view user
-   $scope.viewUser = function () {
-        $scope.modalInstance = $uibModal.open({
-            animation: true,
-            templateUrl: 'views/content/user/modal/viewUser.html',
-            scope: $scope,
-            size: 'md',
-        });
-    };
-  // Delete modal start
-  $scope.deleteItem = function () {
-    $scope.modalInstance = $uibModal.open({
-      animation: true,
-      templateUrl: 'views/modal/delete.html',
-      scope: $scope,
-      size: 'sm',
+
+  // *************************** default variables/tasks begin here ***************** //
+  //- to show/hide sidebar of dashboard 
+  $scope.$parent.isSidebarActive = true;
+  $scope.showSaveBtn = true;
+  $scope.showEditBtn = false;
+
+
+  // *************************** default functions begin here  ********************** //
+  $scope.getUserData = function () {
+    userService.getUserData(function (data) {
+      $scope.userData = data;
     });
+  }
+
+
+  // *************************** functions to be triggered form view begin here ***** //
+
+  $scope.addOrEditUserModal = function (operation, user) {
+    userService.getUserModalData(operation, user, function (data) {
+      $scope.formData = data.user;
+      $scope.showSaveBtn = data.saveBtn;
+      $scope.showEditBtn = data.editBtn;
+
+      $scope.modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'views/content/user/modal/createOrEditUser.html',
+        scope: $scope,
+        size: 'md'
+      });
+
+    });
+  }
+
+  $scope.addOrEditUser = function (userData) {
+    userService.addOrEditUser(userData, function (data) {
+      $scope.operationStatus = "Record added successfully";
+      $scope.getUserData();
+      $scope.cancelModal();  
+    });
+  }
+  //- modal to confirm UOM deletion
+  $scope.deleteUomModal = function (uomId, getFunction) {
+    $scope.idToDelete = uomId;
+    $scope.functionToCall = getFunction;
+
+  }
+  $scope.deleteUom = function (uomId) {
+    baseMatserService.deleteUom(uomId, function (data) {
+
+    });
+  }
+
+
+
+
+  //- to dismiss modal instance
+  $scope.cancelModal = function () {
+    $scope.modalInstance.dismiss();
   };
-  //table data
-  $scope.tableData = [{
-      "id": "1",
-      "name": "kishori",
-      "cname": "1",
-      "dname": "kishori",
-      "cid": "1",
-      "pname": "kishori",
-      "did": "1",
-      "bname": "kishori",
 
-    },
-    {
-      "id": "1",
-      "name": "kishori",
-      "cname": "1",
-      "dname": "kishori",
-      "cid": "1",
 
-    },
-    {
-      "id": "1",
-      "name": "kishori",
-      "cname": "1",
-      "dname": "kishori",
-      "cid": "1",
-    }
-  ]
+  // *************************** init all default functions begin here ************** //
+  //- to initilize the default function 
+  $scope.init = function () {
+    // to get BaseMaster Data
+    $scope.getUserData();
+  }
 
-  
-  //start of pagination 
-  $scope.totalItems = 64;
-  $scope.currentPage = 4;
-
-  $scope.setPage = function (pageNo) {
-    $scope.currentPage = pageNo;
-  };
-
-  $scope.pageChanged = function () {
-    $log.log('Page changed to: ' + $scope.currentPage);
-  };
-
-  $scope.maxSize = 5;
-  $scope.bigTotalItems = 175;
-  $scope.bigCurrentPage = 1;
-
-  //end of pagination
-  //start of checkbox
-  $("#checkAll").click(function () {
-    $('input:checkbox').not(this).prop('checked', this.checked);
-  });
-  // set sidebar closed and body solid layout mode
-  $rootScope.settings.layout.pageContentWhite = true;
-  $rootScope.settings.layout.pageBodySolid = false;
-  $rootScope.settings.layout.pageSidebarClosed = false;
+  $scope.init();
 
 });
