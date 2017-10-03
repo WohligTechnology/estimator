f893274f12e7953ab7be5704f50f2c34887b729e
Pratik Shah <antu.wohligtech@gmail.com>
2017-10-03 15:35:46 +0530

    master process

--- frontend/js/services/master/masterProcessService.js
+++ frontend/js/services/master/masterProcessService.js
@@ -38,28 +38,62 @@ myApp.service('masterProcessService', function ($http, $uibModal, NavigationServ
       callback(data);
     });
   }
-//tree
 
- this.getProcessCatModalData = function (operation, processCat, callback) {
-        var processCatObj = {};
-        if (angular.isDefined(processCat)) {
-           processCatObj.processCat = processCat;
+
+  //- get master process tree structure data
+
+  this.getProcessTreeData = function (callback) {
+    
+      NavigationService.boxCall('MProcessCat/search', function (data) {
+      callback(data.data);
+    });
+  }
+
+
+  this.getProcessCatModalData = function (operation, processCat, callback) {
+    var processCatObj = {};
+    if (angular.isDefined(processCat)) {
+      processCatObj.processCat = processCat;
+    }
+    if (operation == "save") {
+      processCatObj.saveBtn = true;
+      processCatObj.editBtn = false;
+    } else if (operation == "update") {
+      processCatObj.saveBtn = false;
+      processCatObj.editBtn = true;
+    }
+    callback(processCatObj);
+  }
+
+  this.addOrEditProcessCat = function (processCatData, callback) {
+    NavigationService.apiCall('MProcessCat/save', processCatData, function (data) {
+      callback(data);
+    });
+  }
+
+//process Items
+ this.getProcessItemModalData = function (operation, processCatId, processItem, callback) {
+        var processItemObj = {};
+        if (angular.isDefined(processItem)) {
+          processItemObj.processItem = processItem;
         }
         if (operation == "save") {
-           processCatObj.saveBtn = true;
-           processCatObj.editBtn = false;
+          processItemObj.saveBtn = true;
+        processItemObj.editBtn = false;
+          processItemObj.catId = processCatId;
         } else if (operation == "update") {
-          processCatObj.saveBtn = false;
-            processCatObj.editBtn = true;
+          processItemObj.saveBtn = false;
+           processItemObj.editBtn = true;
         }
-        callback(processCatObj);
+        callback(processItemObj);
     }
-
- this.addOrEditProcessCat = function (processCatData, callback) {
-        NavigationService.apiCall('MProcessCat/save', materialCatData, function (data) {
+    this.addOrEditProcessItem = function (processItemData, processCatId, callback) {
+        if (angular.isDefined(processCatId)) {
+            processItemData.catId = processCatId;
+        }
+        NavigationService.apiCall('MProcessItem/save', processItemData, function (data) {
             callback(data);
         });
     }
-
 
 });
