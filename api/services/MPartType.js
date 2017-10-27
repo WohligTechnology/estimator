127a4f8d87ca136e44a14a75ae72606480805471
Vijay Vaidya <vijayvaidya99@gmail.com>
2017-10-27 11:44:30 +0530

    done with add & delete API of partType material, pro, addon, extras

--- api/services/MPartType.js
+++ api/services/MPartType.js
@@ -44,7 +44,7 @@ module.exports = mongoose.model('MPartType', schema);
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema, 'partTypeCat material', 'partTypeCat material'));
 var model = {
 
-    addMaterialToPartType: function (data, callback) {
+    addPartTypeMaterial: function (data, callback) {
         MPartType.findOneAndUpdate({
             _id: data._id,
         }, {
@@ -53,7 +53,7 @@ var model = {
             },
         }).exec(function (err, updatedData) {
             if (err) {
-                console.log('**** error at function_name of MPartType.js ****', err);
+                console.log('**** error at addPartTypeMaterial of MPartType.js ****', err);
                 callback(err, null);
             } else if (_.isEmpty(updatedData)) {
                 callback(null, 'noDataFound');
@@ -63,5 +63,138 @@ var model = {
         });
 
     },
+    addPartTypeProcessing: function (data, callback) {
+        MPartType.findOneAndUpdate({
+            _id: data._id
+        }, {
+            $push: {
+                proccessing: data.proccessingId
+            },
+        }).exec(function (err, updatedData) {
+            if (err) {
+                console.log('**** error at addPartTypeProcessing of MPartType.js ****', err);
+                callback(err, null);
+            } else if (_.isEmpty(updatedData)) {
+                callback(null, 'noDataFound');
+            } else {
+                callback(null, updatedData);
+            }
+        });
+    },
+    addPartTypeAddons: function (data, callback) {
+        MPartType.findOneAndUpdate({
+            _id: data._id
+        }, {
+            $push: {
+                addons: data.addonsId
+            },
+        }).exec(function (err, updatedData) {
+            if (err) {
+                console.log('**** error at addPartTypeAddons of MPartType.js ****', err);
+                callback(err, null);
+            } else if (_.isEmpty(updatedData)) {
+                callback(null, 'noDataFound');
+            } else {
+                callback(null, updatedData);
+            }
+        });
+    },
+    addPartTypeExtras: function (data, callback) {
+        MPartType.findOneAndUpdate({
+            _id: data._id
+        }, {
+            $push: {
+                extras: data.extrasId
+            },
+        }).exec(function (err, updatedData) {
+            if (err) {
+                console.log('**** error at addPartTypeExtras of MPartType.js ****', err);
+                callback(err, null);
+            } else if (_.isEmpty(updatedData)) {
+                callback(null, 'noDataFound');
+            } else {
+                callback(null, updatedData);
+            }
+        });
+    },
+    deletePartTypeMaterial: function (data, callback) {
+        MPartType.findOneAndUpdate({
+            _id: data._id,
+        }, {
+            $pull: {
+                material: data.materialId
+            },
+        }).exec(function (err, updatedData) {
+            if (err) {
+                console.log('**** error at deletePartTypeMaterial of MPartType.js ****', err);
+                callback(err, null);
+            } else if (_.isEmpty(updatedData)) {
+                callback(null, 'noDataFound');
+            } else {
+                callback(null, updatedData);
+            }
+        });
+
+
+    },
+    deleteProcessingPartType: function (data, callback) {
+        MPartType.findOneAndUpdate({
+            _id: data._id
+        }, {
+            $pull: {
+                proccessing: data.proccessingId
+            },
+        }).exec(function (err, updatedData) {
+            if (err) {
+                console.log('**** error at deleteProcessingPartType of MPartType.js ****', err);
+                callback(err, null);
+            } else if (_.isEmpty(updatedData)) {
+                callback(null, 'noDataFound');
+            } else {
+                callback(null, updatedData);
+            }
+        });
+
+    },
+    deleteAddonsPartType: function (data, callback) {
+        MPartType.findOneAndUpdate({
+            _id: data._id
+        }, {
+            $pull: {
+                addons: data.addonsId
+            },
+        }).exec(function (err, updatedData) {
+            if (err) {
+                console.log('**** error at deleteAddonsPartType of MPartType.js ****', err);
+                callback(err, null);
+            } else if (_.isEmpty(updatedData)) {
+                callback(null, 'noDataFound');
+            } else {
+                callback(null, updatedData);
+            }
+        });
+
+    },
+    deleteExtrasPartType: function (data, callback) {
+        MPartType.findOneAndUpdate({
+            _id: data._id
+        }, {
+            $pull: {
+                extras: data.extrasId
+            },
+        }).exec(function (err, updatedData) {
+            if (err) {
+                console.log('**** error at deleteExtrasPartType of MPartType.js ****', err);
+                callback(err, null);
+            } else if (_.isEmpty(updatedData)) {
+                callback(null, 'noDataFound');
+            } else {
+                callback(null, updatedData);
+            }
+        });
+
+    },
+
+
 };
 module.exports = _.assign(module.exports, exports, model);
