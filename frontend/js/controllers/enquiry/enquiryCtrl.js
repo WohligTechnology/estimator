4291aa503e233ed8ad9b61398691613e469cccab
ashish <ashish.zanwar@wohlig.com>
2017-10-27 19:02:57 +0530

    done with add & edit enquiry

--- frontend/js/controllers/enquiry/enquiryCtrl.js
+++ frontend/js/controllers/enquiry/enquiryCtrl.js
@@ -1,51 +1,57 @@
 myApp.controller('enquiryCtrl', function ($scope, $state, $uibModal, enquiryService) {
 
-  $scope.$parent.isSidebarActive = true;
- 
-  //$scope.ID = $routeParams.id;
-  $scope.getCustomerData = function () {
-    enquiryService.getCustomerData(function (data) {
-      $scope.customerData = data;     
-    });
-  }
 
+    // *************************** default variables/tasks begin here ***************** //
+  
+    //- to show/hide sidebar of dashboard   
+  $scope.$parent.isSidebarActive = true;
+
+    // *************************** default functions begin here  ********************** //
+  
+    //to get all enquiries 
   $scope.getEnquiryData = function () {
-    enquiryService.getEnquiryData(function (data) {
+    enquiryService.getEnquiryData(function (data, obj) {
+      $scope.detailsActive = obj.detailsActive;
+      $scope.infoActive = obj.infoActive;
+      $scope.keyReqActive = obj.keyReqActive;
+      $scope.techReqsActive = obj.techReqsActive;
+      $scope.CommReqActive = obj.CommReqActive;
+      $scope.PreQuaCriteriaActive = obj.PreQuaCriteriaActive;
+      
       $scope.tableData = data; 
     });
   }
 
-  // table data
- // $scope.tableData = enquiryService.getEnquiryData();
- 
-  //Edit Enquries Modal
-  $scope.addEnquiryModal = function () {
+    // *************************** functions to be triggered form view begin here ***** //      
+  
+    //- modal to confirm Enquiry deletion
+  $scope.deleteEnquiryModal = function (enquiryId, getFunction) {
+    $scope.idToDelete = enquiryId;
+    $scope.functionToCall = getFunction;
+
     $scope.modalInstance = $uibModal.open({
       animation: true,
-      templateUrl: 'views/content/enquiry/createEnquiry.html',
+      templateUrl: 'views/content/master/base/deleteBaseMasterModal.html',
       scope: $scope,
-      size: 'md',
+      size: 'md'
     });
-  };
-
-
-  $scope.addEnquiry = function(formData,selectedCustomer){
-    formData.customerId = selectedCustomer._id;
-    enquiryService.createEnquiry(formData, function(data){
-      $state.go('app.editEnquiry', {enquiryId:data._id});
+  }
+  $scope.deleteEnquiry = function (enquiryId) {
+    enquiryService.deleteEnquiry(enquiryId, function (data) {
+      $scope.operationStatus = "Record deleted successfully";
+      $scope.cancelModal();
+      $scope.getEnquiryData();
     });
   }
 
+  //cancel modal
   $scope.cancelModal = function () {
       $scope.modalInstance.dismiss();
   }
   
   $scope.init = function () {
-    $scope.getCustomerData();
-    $scope.getEnquiryData();
-    
+    $scope.getEnquiryData();   
   }
-
   $scope.init();
 
 });
--- frontend/js/services/enquiry/createOrEditEnquiryService.js
+++ frontend/js/services/enquiry/createOrEditEnquiryService.js
@@ -1,20 +1,23 @@
 myApp.service('createOrEditEnquiryService', function ($http, NavigationService) {
     
-    // this.getAllEnquiryData = function(){
-    //     NavigationService.boxCall('Enquiry/search', function (data) {
-    //         return data.data.results;
-    //     });
-    // }
-
-    this.getCustomerData = function (callback) {
-        NavigationService.boxCall('Customer/search', function (data) {
-          var customers = data.data.results;
-          callback(customers);
-        });
-      }
-    this.createEnquiry = function (enquiryData, callback) {    
-        NavigationService.apiCall('Enquiry/save', enquiryData, function (data) {
-          callback(data.data);
-        });
-      }
+  this.getEnquiryObj = function (id, callback) {
+    if(angular.isDefined(id)){
+      NavigationService.apiCall('Enquiry/getOne', {_id:id}, function (data) {
+        callback(data.data);
+      });
+    }else{
+      callback();
+    }
+  }
+  this.getCustomerData = function (callback) {
+      NavigationService.boxCall('Customer/search', function (data) {
+        var customers = data.data.results;
+        callback(customers);
+      });
+ } 
+  this.createEnquiry = function (enquiryData, callback) {    
+      NavigationService.apiCall('Enquiry/save', enquiryData, function (data) {
+        callback(data.data);
+      });
+  }
 });    
