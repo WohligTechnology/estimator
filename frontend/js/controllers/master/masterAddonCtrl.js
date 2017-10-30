7ca9e7e54529474053383ce4039090a00b1e7ab6
ashishzanwar <ashish.zanwar@wohlig.com>
2017-10-30 14:19:47 +0530

    doen with addons master

--- frontend/js/controllers/master/masterAddonCtrl.js
+++ frontend/js/controllers/master/masterAddonCtrl.js
@@ -24,13 +24,23 @@ myApp.controller('masterAddonCtrl', function ($scope, $http, $uibModal, masterAd
 
     // *************************** functions to be triggered form view begin here ***** //
     $scope.addOrEditAddonTypeModal = function (operation, addonType) {
-        console.log('**** inside function_name of masterAddonCtrl.js ****',addonType);
+
         masterAddonService.getAddonTypeModalData(operation, addonType, function (data) {
+            console.log('**** inside function_name of masterAddonCtrl.js ****', data);
             $scope.formData = data.addonTypeData;
             $scope.mMatCatData = data.mMatData;
             $scope.mUomData = data.mUomData;
             $scope.showSaveBtn = data.saveBtn;
             $scope.showEditBtn = data.editBtn;
+
+            if (angular.isDefined(data.addonTypeData)) {
+                $scope.selectedMatCat = $scope.formData.materialCat;
+                $scope.selectedMatSubCat = $scope.formData.materialSubCat;
+                $scope.selectedRateUom = $scope.formData.rate.uom;
+                $scope.selectedAdditionalUom = $scope.formData.quantity.additionalInputUom;
+                $scope.selectedKinkedKeyUom = $scope.formData.quantity.linkedKeyUom;
+                $scope.selectedFinalUom = $scope.formData.quantity.finalUom;
+            }
 
             $scope.modalInstance = $uibModal.open({
                 animation: true,
@@ -41,28 +51,43 @@ myApp.controller('masterAddonCtrl', function ($scope, $http, $uibModal, masterAd
         });
     }
 
-    $scope.addOrEditAddonType = function (addonTypeData,selectedMatCatId,selectedMatSubCatId,selectedRateUomId,selectedAdditionalUomId,selectedKinkedKeyUomId,selectedFinalUomId) {
-        console.log('**** inside addonTypeData of masterAddonCtrl.js ****',addonTypeData);
-        console.log('**** inside selectedMatCatId of masterAddonCtrl.js ****',selectedMatCatId);
-        console.log('**** inside selectedMatSubCatId of masterAddonCtrl.js ****',selectedMatSubCatId);
-        console.log('**** inside selectedRateUomId of masterAddonCtrl.js ****',selectedRateUomId);
-        console.log('**** inside selectedAdditionalUomId of masterAddonCtrl.js ****',selectedAdditionalUomId);
-        console.log('**** inside selectedKinkedKeyUomId of masterAddonCtrl.js ****',selectedKinkedKeyUomId);
-        console.log('**** inside selectedFinalUomId of masterAddonCtrl.js ****',selectedFinalUomId);
+    $scope.addOrEditAddonType = function (addonTypeData, selectedMatCatId, selectedMatSubCatId, selectedRateUomId, selectedAdditionalUomId, selectedKinkedKeyUomId, selectedFinalUomId) {
+        addonTypeData.materialCat = selectedMatCatId;
+        addonTypeData.materialSubCat = selectedMatSubCatId;
+        addonTypeData.rate.uom = selectedRateUomId;
+        addonTypeData.quantity.additionalInputUom = selectedAdditionalUomId;
+        addonTypeData.quantity.linkedKeyUom = selectedKinkedKeyUomId;
+        addonTypeData.quantity.finalUom = selectedFinalUomId;
         
-        // masterAddonService.addOrEditAddonType(addonTypeData,);
+        masterAddonService.addOrEditAddonType(addonTypeData, function (data) {
+            $scope.successMessage = "Addon Added successfully..";
+            $scope.getAddonData();
+            $scope.cancelModal();
+        });
     }
-    $scope.deleteAddonTypeModal = function (addonId, getFunction) { }
-    $scope.deleteAddonType = function (addonId) {}
-
-
-
-    //- to push changed value in 
-    $scope.valueChanged = function () {
-
+    $scope.deleteAddonTypeModal = function (addonId, getFunction) {
+        $scope.idToDelete = addonId;
+        $scope.functionToCall = getFunction;
+        $scope.modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'views/content/master/base/deleteBaseMasterModal.html',
+            scope: $scope,
+            size: 'md'
+        });
+    }
+    $scope.deleteAddonType = function (addonId) {
+        masterAddonService.deleteAddonType(addonId, function (data) {
+            $scope.operationStatus = "Record deleted successfully";
+            $scope.getAddonData();
+            $scope.cancelModal();
+        });
     }
 
 
+    //- to dismiss modal instance
+    $scope.cancelModal = function () {
+        $scope.modalInstance.dismiss();
+    };
 
     // *************************** init all default functions begin here ************** //
     //- to initilize the default function 
