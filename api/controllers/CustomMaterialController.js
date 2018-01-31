7837e9b2ace2a87dbf86078863736fc793e1f226
ashishzanwar <ashishzanwar@wohlig.com>
2018-01-31 18:09:58 +0530

    created api for create multiple custom material while importing in another estimate

--- api/controllers/CustomMaterialController.js
+++ api/controllers/CustomMaterialController.js
@@ -40,5 +40,35 @@ var controller = {
             })
         }
     },
+
+    saveImportedCustMat: function (req, res) {
+        if (req.body) {
+            // please remove Controller.js from below line
+            var savedCustMats = [];
+            async.eachSeries(req.body.cmArray, function (cmObj, callback) {
+
+                CustomMaterial.createCustomMat(cmObj, function (err, data) {
+                    console.log('**** inside function_name of CustomMaterialController.js ****',data);
+                    savedCustMats.push(data);
+                    callback();
+                });
+
+            }, function (err) {
+                if (err) {
+                    console.log('***** error at final response of async.eachSeries in function_name of CustomMaterialController.js*****', err);
+                } else {
+                    res.callback(null, savedCustMats);
+                }
+            });
+        } else {
+            res.json({
+                value: false,
+                data: {
+                    message: 'Invalid Request'
+                }
+            })
+        }
+    },
+
 };
 module.exports = _.assign(module.exports, controller);
