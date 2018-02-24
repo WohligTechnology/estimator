67b435f711e1b1bb9ac23e1961bbf3adde396b68
Vijay Vaidya <vijayvaidya99@gmail.com>
2018-02-24 16:23:03 +0530

    changes for delRestrictionOfMPartTypeCat

--- api/services/MPartTypeCat.js
+++ api/services/MPartTypeCat.js
@@ -34,17 +34,67 @@ var model = {
     // what this function will do ?
     // req data --> ?
     delRestrictionOfMPartTypeCat: function (data, callback) {
-        MPartTypeCat.find({
-            partTypes: data.partTypes
-        }).exec(function (err, found) {
-            console.log('**** found found ****', found);
+        MPartTypeCat.findOne({
+            _id: data._id
+        }).lean().exec(function (err, found) {
             if (err) {
                 console.log('**** error at function_name of MPartTypeCat.js ****', err);
                 callback(err, null);
             } else if (_.isEmpty(found)) {
-                callback(null, 'noDataFound');
+                callback(null, []);
             } else {
-                callback(null, found);
+                console.log('****found.partTypesfound.partTypes ****', found.partTypes);
+                EstimatePart.find({
+                    partType: found.partTypes
+                }).lean().exec(function (err, partData) {
+                    if (err) {
+                        console.log('**** error at function_name of MPartTypeCat.js ****', err);
+                        callback(err, null);
+                    } else if (_.isEmpty(partData)) {
+                        async.parallel([
+                            function (callback) {
+                                MPartType.remove({
+                                    _id: found.partTypes
+                                }).exec(function (err, mPartTypeRemoved) {
+                                    console.log('****part typeppp ****', mPartTypeRemoved);
+                                    if (err) {
+                                        console.log('**** error at function_name of MPartTypeCat.js ****', err);
+                                        callback(err, null);
+                                    } else if (_.isEmpty(mPartTypeRemoved)) {
+                                        callback(null, 'noDataFound');
+                                    } else {
+                                        callback(null, mPartTypeRemoved);
+                                    }
+                                });
+                            },
+                            function (callback) {
+                                MPartPresets.remove({
+                                    partType: found.partTypes
+                                }).exec(function (err, mPartPresetsData) {
+                                    if (err) {
+                                        console.log('**** error at function_name of MPartTypeCat.js ****', err);
+                                        callback(err, null);
+                                    } else if (_.isEmpty(mPartPresetsData)) {
+                                        callback(null, 'noDataFound');
+                                    } else {
+                                        callback(null, mPartPresetsData);
+                                    }
+                                });
+                            },
+                        ], function (err, finalResults) {
+                            if (err) {
+                                console.log('********** error at final response of async.parallel  MPartTypeCat.js ************', err);
+                                callback(err, null);
+                            } else if (_.isEmpty(finalResults)) {
+                                callback(null, 'noDataFound');
+                            } else {
+                                callback(null, finalResults);
+                            }
+                        });
+                    } else {
+                        callback(null, 'dependecy of table estimate part');
+                    }
+                });
             }
         });
     },
